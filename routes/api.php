bfe19b57e78a71278c75e31793def26704a0df6a
islamhanii <islamhani433@gmail.com>
2022-08-23 19:52:38 +0200

    No change re-initiate to change storage path

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('test', [TestController::class, 'test']);
