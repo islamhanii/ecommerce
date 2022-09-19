5d66e105b9bdbb8929efb84b396875d222f37761
islamhanii <islamhani433@gmail.com>
2022-09-19 18:34:53 +0200

    Add code to products Table

--- database/migrations/2022_09_18_143129_add_code_to_products_table.php
+++ database/migrations/2022_09_18_143129_add_code_to_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddNullToMainImageInProducts extends Migration
+class AddCodeToProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddNullToMainImageInProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('code')->unique()->nullable()->after('sub_category_id');
         });
     }
 
@@ -26,7 +26,7 @@ class AddNullToMainImageInProducts extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('main_image')->nullable()->change();
+            $table->dropColumn('code');
         });
     }
 }
